2f62f30b1929404a3c646454a4e0d51d8e8d3c6e
Colton Jones <coltoncejones12@gmail.com>
2019-04-02 18:52:52 -0400

    js finalizing

--- Portfolio.js
+++ Portfolio.js
@@ -8,15 +8,20 @@ $.fn.isInViewport = function() {
     return elementBottom > viewportTop && elementTop < viewportBottom;
 };
   
-$(window).on('resize scroll', function() {
-  $('.container').each(function() {
-      var activeContainer = $(this).attr('id');
-      if ($(this).isInViewport()) {
-          if ($('h1').parent('a').hasClass(activeContainer))
-                $('h1').parent('a').addClass("active");
-      } else {
-          if ($('h1').parent('a').hasClass(activeContainer))
-                $('h1').parent('a').removeClass("active");
-    }
+$(window).on('resize scroll', function(e) {
+    $('.container').each(function() {
+      if ($('#bio').isInViewport()) {
+        $('a[href$="#bio"]').addClass('active')
+        $('a[href$="#projects"]').removeClass('active')
+        $('a[href$="#contact"]').removeClass('active')
+      } else if ($('#projects').isInViewport()) {
+        $('a[href$="#bio"]').removeClass('active')
+        $('a[href$="#projects"]').addClass('active')
+        $('a[href$="#contact"]').removeClass('active')
+      } else if ($('#contact').isInViewport()) {
+        $('a[href$="#bio"]').removeClass('active')
+        $('a[href$="#projects"]').removeClass('active')
+        $('a[href$="#contact"]').addClass('active')
+      }
+    });
   });
-});
